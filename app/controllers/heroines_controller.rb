8e735ada74951d37be055bb57a216fa8ecee5376
Naomi Katz <naykatz@gmail.com>
2019-03-21 10:52:52 -0400

    i did it

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,42 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(strong_heroine_params)
+    if @heroine.valid?
+     redirect_to heroines_path
+   else
+     flash[:errors] = @heroine.errors.full_messages
+     redirect_to new_heroine_path
+   end
+  end
+
+
+  def edit
+      @heroine = Heroine.find(params[:id])
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id])
+    @heroine.update(strong_heroine_params)
+    redirect_to heroines_path
+  end
+
+
+  private
+  def strong_heroine_params
+    params.require(:heroine).permit(:name, :super_name)
+  end
+
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,38 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.create(strong_power_params)
+     redirect_to new_power_path
+  end
+
+
+  def edit
+      @power = Power.find(params[:id])
+  end
+
+  def update
+    @power = Power.find(params[:id])
+    @power.update(strong_power_params)
+    redirect_to powers_path
+  end
+
+
+private
+def strong_power_params
+  params.require(:power).permit(:name, :description)
+end
+
+
+
 end
