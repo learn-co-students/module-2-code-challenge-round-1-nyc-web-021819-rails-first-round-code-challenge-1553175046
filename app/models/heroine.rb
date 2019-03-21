9f7d86279920447a8e21477001397d09c4c844ee
richietam <richardtam.ny@gmail.com>
2019-03-21 10:52:00 -0400

    completed code Challenge

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,21 @@
 class Heroine < ApplicationRecord
+
+  belongs_to :power
+  validates :super_name, uniqueness: true
+
+  # def self.search(search)
+  #   if search
+  #     power = Power.find_by(name: search)
+  #     if power
+  #       self.where(power_id: power)
+  #     else
+  #       Heroine.all
+  #     end
+  #     Heroine.all
+  #   end
+  # end
+
 end
+
+
+# rails generate migration add_email_to_users email:string
