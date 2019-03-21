9e0b10bfa8e951110fed4b5656aae2b157e05746
github username <github email address>
2019-03-21 10:53:15 -0400

    I did it

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,11 @@
 class Power < ApplicationRecord
+  has_many :heroines, dependent: :destroy
+  
+  # def heroines_with_power
+  #   self.heroines.each do |heroine|
+  #     heorine.super_name
+  #   end
+  # end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:index, :show]
+  resources :heroines, only: [:index, :show, :new, :create]
+  # get "/heroines/:id", to: "heroines#show"
+
+  # get "/search", to: "powers#find_by_power", as: "find_by_power"
+
 end
