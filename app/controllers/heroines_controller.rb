22bee10fd7626fa4982df716eaad4262c124d223
github username <github email address>
2019-03-21 10:52:14 -0400

    finished code challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,35 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    @powers = Power.all
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    render :show
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+    render :new
+  end
+
+  def create
+    heroine = Heroine.create(heroine_params)
+    if heroine.valid?
+      redirect_to heroines_path
+    else
+      flash[:no_unique] = heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,13 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  # def search
+  #   # @power = Power.find(params[:id])
+  #   render :search
+  # end
 end
