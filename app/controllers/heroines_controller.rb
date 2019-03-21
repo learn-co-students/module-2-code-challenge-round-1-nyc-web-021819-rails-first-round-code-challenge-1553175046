50b14185f9a0866bb9af16a3c7adcf5d59da8b30
Jessy Hu <jesshuy12@gmail.com>
2019-03-21 10:44:45 -0400

    everything but bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    heroine = Heroine.create(heroine_param)
+    if heroine.valid?
+    redirect_to heroines_path
+    else
+      flash[:heroine_errors] = heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+  #issue showing the power, i used power
+
+  private
+
+  def heroine_param
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
