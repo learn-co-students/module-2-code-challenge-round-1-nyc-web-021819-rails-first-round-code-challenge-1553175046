99f4df0ec210b592f31cae148ebdff3ac06956c3
github username <github email address>
2019-03-21 10:52:01 -0400

    done

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', '~> 1.3.6'
 # Use Puma as the app server
 gem 'puma'
 # Use SCSS for stylesheets
@@ -20,7 +20,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'rb-readline'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,40 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    @heroines_search = Heroine.search(params[:search])
+    render :index
   end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+    render :new
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    render :show
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path(@heroine)
+    end
+  end
+
+
+
+
+ private
+
+ def heroine_params
+   params.require(:heroine).permit(:name, :super_name, :power_id, :search)
+
+ end
+
+
 end
